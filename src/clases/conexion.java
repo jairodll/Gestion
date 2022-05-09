3f434b9485489d3cdd31fdb369e6a06c52b15ea5
Jairo de León <Jairo de Len@JAIRO>
2022-05-08 21:26:10 -0600

    cambios 08-05-2022

--- src/clases/conexion.java
+++ src/clases/conexion.java
@@ -17,18 +17,9 @@ public class conexion {
         }
         return (null);
         } 
-    /* public static void main(String[] args){
-        String BD ="jdbc:postgresql://127.0.0.1:5432/recursosc";
-        String usuario = "postgres";
-        String contra ="admin";
-        
-        try {
-            Connection conectar = DriverManager.getConnection(BD,usuario,contra);
-            JOptionPane.showMessageDialog(null, "BD Conectada con exito");
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Error al conectar"+e);
- 
-        }
+    /* PRUEBA PARA GITHUB
+    :D
+    :D
     }   */    
     
     
